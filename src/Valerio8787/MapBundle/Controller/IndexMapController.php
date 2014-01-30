c732b8957ae524831fa3e8bbc6bc345cff5657bc
valerio8787 <valerio8787@gmail.com>
2014-01-30 07:01:23 +0200

    adding index map

---
+++ src/Valerio8787/MapBundle/Controller/IndexMapController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Valerio8787\MapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class IndexMapController extends Controller {
+
+    //Менеджер сутностей
+    private $em;
+
+    /**
+     * @Route("/index")
+     * @Template()
+     */
+    public function indexAction() {
+        $this->em = $this->getDoctrine()->getManager();
+        $poses = $this->em->getRepository('Valerio8787SchemaBundle:Pos')->createQueryBuilder('p')
+                        ->where('p.id in (310,311,312,313,314,315,316,317)')
+                        ->getQuery()->getArrayResult();
+        $routes = $this->em->getRepository('Valerio8787SchemaBundle:PosToPos')->createQueryBuilder('ptp')
+                        ->select('ptp.distance, ptp.route, pf.id as pfrom, pt.id as pto')
+                        ->innerjoin('ptp.posFrom', 'pf')
+                        ->innerjoin('ptp.posTo', 'pt')
+                        ->where('pf.id in (310,311,312,313,314,315,316,317) AND pt.id in (310,311,312,313,314,315,316,317)')
+                        ->getQuery()->getArrayResult();
+
+        return(array('poses' => json_encode($poses),
+            'routes' => json_encode($routes)));
+    }
+
+}
---
+++ src/Valerio8787/MapBundle/Valerio8787MapBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Valerio8787\MapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Valerio8787MapBundle extends Bundle
+{
+}
