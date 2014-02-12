599f318e5d02824f1bf30b55d6f6b31ba0885d33
valerio8787 <valerio8787@gmail.com>
2014-02-12 07:01:47 +0200

    local optimize

--- src/Valerio8787/OptimizeBundle/Optimizer/TravelingSalesmanOptimizer.php
+++ src/Valerio8787/OptimizeBundle/Optimizer/TravelingSalesmanOptimizer.php
@@ -31,10 +31,35 @@ class TravelingSalesmanOptimizer extends AbstractOptimizer {
             $change = false;
             for ($i = 0; $i < $n - 1; $i++) {
                 for ($j = $i + 1; $j < $n; $i++) {
-                    $change = true;
+                    if ($i = $j + 1) {
+                        if ($this->best1($i, $j)) {
+                            $this->swap($i, $j);
+                        }
+                    } else if (($i = 1) && ($j = $n)) {
+                        if ($this->best1($i, $j)) {
+                            $this->swap($i, $j);
+                        }
+                    } else
+                    if ($this->best2($i, $j)) {
+                        $this->swap($i, $j);
+                    }
                 }
             }
         } while ($change);
+    }
+
+    private function best1($i, $j) {
+        return (($this->dm[$i - 1][$i] + $this->dm[$i][$j] + $this->dm[$j][$j + 1]) >
+                ($this->dm[$i - 1][$j] + $this->dm[$j][$i] + $this->dm[$i][$j + 1]));
+    }
+
+    private function best2($i, $j) {
+        return (($this->dm[$i - 1][$i] + $this->dm[$i][$i + 1] + $this->dm[$j - 1][$j] + $this->dm[$j][$j + 1]) >
+                ($this->dm[$i - 1][$j] + $this->dm[$j][$i + 1] + $this->dm[$j - 1][$i] + $this->dm[$i][$j + 1]));
+    }
+
+    private function swap($i, $j) {
+        
     }
 
     protected function optimize() {
